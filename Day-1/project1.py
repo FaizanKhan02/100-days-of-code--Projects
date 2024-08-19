c3d5ce6662bf9486879b8f23f289f333583739d3
Faizan Khan <fk1942002@gmail.com>
2024-08-19 16:40:53 +0530

    Add Day 1 and Day 2 project files and update README.md
    - Added project1.py under Day-1 directory
    - Added project2.py under Day-2 directory
    - Updated README.md with relevant information

---
+++ Day-1/project1.py
@@ -0,0 +1,6 @@
+# print function, input function, variables
+print("Welcome to the Band Name Generator")
+city = input("In which city do you live?\n")
+pet_name = input("What is your pet name?\n")
+
+print (city + " " + pet_name)
